fafcd2f2f8cefdd9c4fef2b8ad4cecb8f9b468a2
dkoleksandr <didenk.oleksandr@gmail.com>
2024-05-13 22:48:43 +0300

    prettier formatting

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,7 +3,6 @@ import css from "./FriendList.module.css";
 import FriendListItem from "../FriendListItem/FriendListItem";
 
 const FriendList = ({ friends }) => {
-
   return (
     <ul className={css.friendList}>
       {friends.map((friend) => {
